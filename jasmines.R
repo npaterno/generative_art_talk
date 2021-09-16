df5396f9ed12ddacf449d5268c34dc3fe1e0d2bd
npaterno <nicholas.paterno@gmail.com>
2021-09-16 12:24:15 -0700

    final(?) set of slides

--- jasmines.R
+++ jasmines.R
@@ -1,23 +1,27 @@
 library(jasmines)
 library(dplyr)
+library(ggplot2)
 
 use_seed(1) %>%
   scene_discs(
-    rings = 3, points = 5000, size = 5
+    rings = 3, points = 6000, size = 4
   ) %>%
   mutate(ind = 1:n()) %>%
   unfold_warp(
-    iterations = 1,
-    scale = .5, 
+    iterations = 2,
+    scale = .6, 
     output = "layer" 
   ) %>%
   unfold_tempest(
-    iterations = 20,
+    iterations = 25,
     scale = .01
   ) %>%
   style_ribbon(
-    palette = palette_named("vik"),
-    colour = "ind",
-    alpha = c(.1,.1),
+    palette = "viridis",
+    colour = "layer",
+    alpha = c(.15,.05),
     background = "oldlace"
   )
+
+# Save image
+ggsave(here::here("images/jasmines.png"), dpi = 300)
